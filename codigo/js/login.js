8091418b957ecd2991606f6a367d95e7353b8faf
Kadu Bellini <118315169+kadubxllini@users.noreply.github.com>
2026-04-17 13:59:28 +0000

    terminei a parte de login e organizei o código

---
+++ codigo/js/login.js
@@ -0,0 +1,15 @@
+function fazerLogin(event) {
+    
+    event.preventDefault(); // corrige um erro que atualiza a página ao invés de carregar a nova página
+    
+    const user = document.getElementById('js-user').value;
+    const senha = document.getElementById('js-senha').value;
+
+    if (user === 'login' && senha === 'senha') {
+        window.location.href = 'loja/loja.html';
+
+    } else {
+        alert('E-mail ou senha inválidos');
+    }
+
+}
